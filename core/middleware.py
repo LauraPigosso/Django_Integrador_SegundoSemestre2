a7f7cf3d22a9a77171a20aa08df701a3c28e2cec
Laura Pigosso <127753547+LauraPigosso@users.noreply.github.com>
2023-12-07 16:56:46 -0300

    Add files via upload

--- core/middleware.py
+++ core/middleware.py
@@ -5,29 +5,29 @@ from rest_framework import status
 from django.utils import timezone
 from django.http import JsonResponse
 
-""" Middleware that is called on each request
+""" Middleware que é chamado a cada requisição
 """
 class LoginAttemptsMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request: WSGIRequest): 
-        # get body of the request
+        # Obter corpo da requisição
         body = request.body
 
-        # run view functions
+        # Executar funções de visualização
         response = self.get_response(request)
 
-        # verify path if is 'api/token'
+        # Verificar se o caminho é 'api/token'
         if request.path == "/api/token/":
             
-            # get content of request
+            # Obter conteúdo da requisição
             content_type = request.headers.get("Content-Type", '').lower()
 
-            # Getting email or None - in forms or jsons
+            # Obter email ou None - em formulários ou JSON
             if 'application/json' in content_type:
 
-                # if the content is a json:
+                # Se o conteúdo for um JSON:
                 try:
                     email = json.loads(body.decode('utf-8')).get('email')
                 except json.JSONDecodeError:
@@ -35,40 +35,40 @@ class LoginAttemptsMiddleware:
 
             elif 'application/x-www-form-urlencoded' in content_type:
                 
-                # if the content is a form:
+                # Se o conteúdo for um formulário:
                 email = request.POST.get('email')
             
             else:
                 email = None
 
-            # if email recieved:
+            # Se o email for recebido:
             if email:
                 user = User.objects.get(email=email)
 
-                # if user was created less than 3 minutes ago
+                # Se o usuário foi criado há menos de 3 minutos
                 if timezone.now() <= (user.created_at + timezone.timedelta(minutes=3)):
                     return JsonResponse(
-                    {'detail': 'Your account is in analysis. Try again later'},
+                    {'detail': 'Sua conta está em análise. Tente novamente mais tarde'},
                     status=status.HTTP_401_UNAUTHORIZED
                 )    
 
-                # if login is wrong:
+                # Se o login estiver errado:
                 if user and response.status_code == status.HTTP_401_UNAUTHORIZED:
                     user.login_attempts += 1
                     user.save()
 
-                    # if 3 wrong attempts, lock user for 15 minutes
-                    if user.login_attempts ==3:
+                    # Se 3 tentativas erradas, bloquear o usuário por 15 minutos
+                    if user.login_attempts == 3:
                         user.locked_at = timezone.now()
                         user.unlocked_at = timezone.now() + timezone.timedelta(minutes=15)
                         user.save()
                         
                         return JsonResponse(
-                            {'detail': 'Blocked account. Try again in 15 minutes'},
+                            {'detail': 'Conta bloqueada. Tente novamente em 15 minutos'},
                             status=status.HTTP_401_UNAUTHORIZED
                         )
                 
-                if user.login_attempts >= 3 and user.locked_at != None and user.unlocked_at != None and status.HTTP_200_OK:
+                if user.login_attempts >= 3 and user.locked_at is not None and user.unlocked_at is not None and response.status_code == status.HTTP_200_OK:
                     if timezone.now() >= user.unlocked_at:
                         user.login_attempts = 0
                         user.locked_at = None
@@ -76,7 +76,7 @@ class LoginAttemptsMiddleware:
                         user.save()
                     else:
                         return JsonResponse(
-                            {'detail': 'Your account has been blocked. Try again later'},
+                            {'detail': 'Sua conta foi bloqueada. Tente novamente mais tarde'},
                             status=status.HTTP_418_IM_A_TEAPOT
                         )
         
